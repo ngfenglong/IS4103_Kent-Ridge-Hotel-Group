769c841b9d0916d94957a052a37cdecd0c02e9be
Derek <dk3498@gmail.com>
2019-02-09 13:54:01 +0800

    Added LostAndFoundReport Entity

--- KRHG/KRHG-ejb/src/java/entity/LostAndFoundReport.java
+++ KRHG/KRHG-ejb/src/java/entity/LostAndFoundReport.java
@@ -6,10 +6,14 @@
 package entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 /**
  *
@@ -21,20 +25,29 @@ public class LostAndFoundReport implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    private Long reportID;
+    private String itemName;
+    @Temporal(TemporalType.DATE)
+    private Date reportedDate;
+    private String itemDescription;
+    private String contactNum;
+    private String reportType;
+    private Boolean isResolved;
+    private ArrayList<String> keywords;
+    private String itemImage;
 
-    public Long getId() {
-        return id;
+    public Long getReportId() {
+        return reportID;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setReportId(Long reportID) {
+        this.reportID = reportID;
     }
 
     @Override
     public int hashCode() {
         int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
+        hash += (reportID != null ? reportID.hashCode() : 0);
         return hash;
     }
 
@@ -45,7 +58,7 @@ public class LostAndFoundReport implements Serializable {
             return false;
         }
         LostAndFoundReport other = (LostAndFoundReport) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+        if ((this.reportID == null && other.reportID != null) || (this.reportID != null && !this.reportID.equals(other.reportID))) {
             return false;
         }
         return true;
@@ -53,7 +66,118 @@ public class LostAndFoundReport implements Serializable {
 
     @Override
     public String toString() {
-        return "entity.LostAndFoundReport[ id=" + id + " ]";
+        return "entity.LostAndFoundReport[ reportID=" + reportID + " ]";
     }
     
+     /**
+     * @return the itemName
+     */
+    public String getItemName() {
+        return itemName;
+    }
+
+    /**
+     * @param itemName the itemName to set
+     */
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    /**
+     * @return the reportedDate
+     */
+    public Date getReportedDate() {
+        return reportedDate;
+    }
+
+    /**
+     * @param reportedDate the reportedDate to set
+     */
+    public void setReportedDate(Date reportedDate) {
+        this.reportedDate = reportedDate;
+    }
+
+    /**
+     * @return the itemDescription
+     */
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    /**
+     * @param itemDescription the itemDescription to set
+     */
+    public void setItemDescription(String itemDescription) {
+        this.itemDescription = itemDescription;
+    }
+
+    /**
+     * @return the contactNum
+     */
+    public String getContactNum() {
+        return contactNum;
+    }
+
+    /**
+     * @param contactNum the contactNum to set
+     */
+    public void setContactNum(String contactNum) {
+        this.contactNum = contactNum;
+    }
+
+    /**
+     * @return the reportType
+     */
+    public String getReportType() {
+        return reportType;
+    }
+
+    /**
+     * @param reportType the reportType to set
+     */
+    public void setReportType(String reportType) {
+        this.reportType = reportType;
+    }
+
+    /**
+     * @return the isResolved
+     */
+    public Boolean getIsResolved() {
+        return isResolved;
+    }
+
+    /**
+     * @param isResolved the isResolved to set
+     */
+    public void setIsResolved(Boolean isResolved) {
+        this.isResolved = isResolved;
+    }
+
+    /**
+     * @return the keywords
+     */
+    public ArrayList<String> getKeywords() {
+        return keywords;
+    }
+
+    /**
+     * @param keywords the keywords to set
+     */
+    public void setKeywords(ArrayList<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    /**
+     * @return the itemImage
+     */
+    public String getItemImage() {
+        return itemImage;
+    }
+
+    /**
+     * @param itemImage the itemImage to set
+     */
+    public void setItemImage(String itemImage) {
+        this.itemImage = itemImage;
+    }
 }
