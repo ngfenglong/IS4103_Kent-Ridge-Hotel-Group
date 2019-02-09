ce4219e7eb751680dcbf59dbd8e234d8f97d1afa
Derek <dk3498@gmail.com>
2019-02-09 14:11:17 +0800

    Added WorkSchedule Entity

--- KRHG/KRHG-ejb/src/java/entity/WorkSchedule.java
+++ KRHG/KRHG-ejb/src/java/entity/WorkSchedule.java
@@ -6,46 +6,54 @@
 package entity;
 
 import java.io.Serializable;
+import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 /**
  *
  * @author fengl
  */
 @Entity
-public class WorkingSchedule implements Serializable {
-
+public class WorkSchedule implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    private Long workScheduleID;
+    @Temporal(TemporalType.DATE)
+    private Date workScheduleDate;
+    private int workScheduleSlot;
+    private String approvedStatus;
+    private Staff approver;
+    private Staff allocatedTo;
 
-    public Long getId() {
-        return id;
+    public Long getWorkScheduleId() {
+        return workScheduleID;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setWorkScheduleId(Long workScheduleID) {
+        this.workScheduleID = workScheduleID;
     }
 
     @Override
     public int hashCode() {
         int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
+        hash += (workScheduleID != null ? workScheduleID.hashCode() : 0);
         return hash;
     }
 
     @Override
     public boolean equals(Object object) {
         // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof WorkingSchedule)) {
+        if (!(object instanceof WorkSchedule)) {
             return false;
         }
-        WorkingSchedule other = (WorkingSchedule) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+        WorkSchedule other = (WorkSchedule) object;
+        if ((this.workScheduleID == null && other.workScheduleID != null) || (this.workScheduleID != null && !this.workScheduleID.equals(other.workScheduleID))) {
             return false;
         }
         return true;
@@ -53,7 +61,77 @@ public class WorkingSchedule implements Serializable {
 
     @Override
     public String toString() {
-        return "entity.WorkingSchedule[ id=" + id + " ]";
+        return "entity.WorkingSchedule[ workScheduleID=" + workScheduleID + " ]";
+    }
+    
+     /**
+     * @return the workScheduleDate
+     */
+    public Date getWorkScheduleDate() {
+        return workScheduleDate;
+    }
+
+    /**
+     * @param workScheduleDate the workScheduleDate to set
+     */
+    public void setWorkScheduleDate(Date workScheduleDate) {
+        this.workScheduleDate = workScheduleDate;
+    }
+
+    /**
+     * @return the workScheduleSlot
+     */
+    public int getWorkScheduleSlot() {
+        return workScheduleSlot;
+    }
+
+    /**
+     * @param workScheduleSlot the workScheduleSlot to set
+     */
+    public void setWorkScheduleSlot(int workScheduleSlot) {
+        this.workScheduleSlot = workScheduleSlot;
+    }
+
+    /**
+     * @return the approvedStatus
+     */
+    public String getApprovedStatus() {
+        return approvedStatus;
+    }
+
+    /**
+     * @param approvedStatus the approvedStatus to set
+     */
+    public void setApprovedStatus(String approvedStatus) {
+        this.approvedStatus = approvedStatus;
+    }
+
+    /**
+     * @return the approver
+     */
+    public Staff getApprover() {
+        return approver;
+    }
+
+    /**
+     * @param approver the approver to set
+     */
+    public void setApprover(Staff approver) {
+        this.approver = approver;
+    }
+
+    /**
+     * @return the allocatedTo
+     */
+    public Staff getAllocatedTo() {
+        return allocatedTo;
+    }
+
+    /**
+     * @param allocatedTo the allocatedTo to set
+     */
+    public void setAllocatedTo(Staff allocatedTo) {
+        this.allocatedTo = allocatedTo;
     }
     
 }
