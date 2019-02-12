25baf0eff3c272938b1f134552c4977b3c41a75f
anselmdm <deadapple@live.com>
2019-02-12 16:55:15 +0800

    laundryorder entity

--- KRHG/KRHG-ejb/src/java/entity/LaundryOrder.java
+++ KRHG/KRHG-ejb/src/java/entity/LaundryOrder.java
@@ -6,10 +6,13 @@
 package entity;
 
 import java.io.Serializable;
+import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 /**
  *
@@ -21,20 +24,28 @@ public class LaundryOrder implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    private Long laundryOrderID;
+    private Room room;
+    @Temporal(TemporalType.DATE)
+    private Date orderDateTime;
+    private String status;
+    private Date completeDateTime;
+    private Staff houseKeeper;
+    private String specialRequest;
+    private int qty;
 
-    public Long getId() {
-        return id;
+    public Long getLaundryOrderID() {
+        return laundryOrderID;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setLaundryOrderID(Long laundryOrderID) {
+        this.laundryOrderID = laundryOrderID;
     }
 
     @Override
     public int hashCode() {
         int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
+        hash += (laundryOrderID != null ? laundryOrderID.hashCode() : 0);
         return hash;
     }
 
@@ -45,7 +56,7 @@ public class LaundryOrder implements Serializable {
             return false;
         }
         LaundryOrder other = (LaundryOrder) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+        if ((this.laundryOrderID == null && other.laundryOrderID != null) || (this.laundryOrderID != null && !this.laundryOrderID.equals(other.laundryOrderID))) {
             return false;
         }
         return true;
@@ -53,7 +64,63 @@ public class LaundryOrder implements Serializable {
 
     @Override
     public String toString() {
-        return "entity.LaundryOrder[ id=" + id + " ]";
+        return "entity.LaundryOrder[ laundryOrderID=" + laundryOrderID + " ]";
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public Date getOrderDateTime() {
+        return orderDateTime;
+    }
+
+    public void setOrderDateTime(Date orderDateTime) {
+        this.orderDateTime = orderDateTime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getCompleteDateTime() {
+        return completeDateTime;
+    }
+
+    public void setCompleteDateTime(Date completeDateTime) {
+        this.completeDateTime = completeDateTime;
+    }
+
+    public Staff getHouseKeeper() {
+        return houseKeeper;
+    }
+
+    public void setHouseKeeper(Staff houseKeeper) {
+        this.houseKeeper = houseKeeper;
+    }
+
+    public String getSpecialRequest() {
+        return specialRequest;
+    }
+
+    public void setSpecialRequest(String specialRequest) {
+        this.specialRequest = specialRequest;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
     }
     
 }
