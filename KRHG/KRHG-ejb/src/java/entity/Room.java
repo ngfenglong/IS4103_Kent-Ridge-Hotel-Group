4a3b79eb2d30edb772df06dd64c3cb6442831ce5
Derek <dk3498@gmail.com>
2019-02-16 16:16:43 +0800

    Added Room Entity

--- KRHG/KRHG-ejb/src/java/entity/Room.java
+++ KRHG/KRHG-ejb/src/java/entity/Room.java
@@ -6,35 +6,37 @@
 package entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-/**
- *
- * @author fengl
- */
 @Entity
 public class Room implements Serializable {
-
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    private Long roomID;
+    private String roomName;
+    private String roomType;
+    private String roomPax;
+    private String roomHotel;
+    private ArrayList<RoomFacility> roomFacilities;
+    private String status;
 
-    public Long getId() {
-        return id;
+    public Long getRoomID() {
+        return roomID;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setRoomID(Long roomID) {
+        this.roomID = roomID;
     }
 
     @Override
     public int hashCode() {
         int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
+        hash += (roomID != null ? roomID.hashCode() : 0);
         return hash;
     }
 
@@ -45,7 +47,7 @@ public class Room implements Serializable {
             return false;
         }
         Room other = (Room) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+        if ((this.roomID == null && other.roomID != null) || (this.roomID != null && !this.roomID.equals(other.roomID))) {
             return false;
         }
         return true;
@@ -53,7 +55,92 @@ public class Room implements Serializable {
 
     @Override
     public String toString() {
-        return "entity.Room[ id=" + id + " ]";
+        return "entity.Room[ roomID=" + roomID + " ]";
+    }
+    
+    
+    /**
+     * @return the roomName
+     */
+    public String getRoomName() {
+        return roomName;
+    }
+
+    /**
+     * @param roomName the roomName to set
+     */
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    /**
+     * @return the roomType
+     */
+    public String getRoomType() {
+        return roomType;
+    }
+
+    /**
+     * @param roomType the roomType to set
+     */
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    /**
+     * @return the roomPax
+     */
+    public String getRoomPax() {
+        return roomPax;
+    }
+
+    /**
+     * @param roomPax the roomPax to set
+     */
+    public void setRoomPax(String roomPax) {
+        this.roomPax = roomPax;
+    }
+
+    /**
+     * @return the roomHotel
+     */
+    public String getRoomHotel() {
+        return roomHotel;
+    }
+
+    /**
+     * @param roomHotel the roomHotel to set
+     */
+    public void setRoomHotel(String roomHotel) {
+        this.roomHotel = roomHotel;
+    }
+
+    /**
+     * @return the roomFacilities
+     */
+    public ArrayList<RoomFacility> getRoomFacilities() {
+        return roomFacilities;
+    }
+
+    /**
+     * @param roomFacilities the roomFacilities to set
+     */
+    public void setRoomFacilities(ArrayList<RoomFacility> roomFacilities) {
+        this.roomFacilities = roomFacilities;
+    }
+
+    /**
+     * @return the status
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(String status) {
+        this.status = status;
     }
     
 }
