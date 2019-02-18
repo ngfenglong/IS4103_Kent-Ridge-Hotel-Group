3cd3ceefda2ade58465678dc119f118b5bb82ee1
Feng Long <fenglong1502@Hotmail.com>
2019-02-18 11:10:46 +0800

    commit holiday surcharge entity

--- KRHG/KRHG-ejb/src/java/entity/HolidaySurcharge.java
+++ KRHG/KRHG-ejb/src/java/entity/HolidaySurcharge.java
@@ -6,6 +6,7 @@
 package entity;
 
 import java.io.Serializable;
+import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -21,20 +22,50 @@ public class HolidaySurcharge implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
-    public Long getId() {
-        return id;
+    private Long holidaySurchargeID;
+    private String holidayName;
+    private Date holidayDate;
+    private double holidaySurchargePrice;
+    
+    
+    public Long getHolidaySurchargeID() {
+        return holidaySurchargeID;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setHolidaySurchargeID(Long holidaySurchargeID) {
+        this.holidaySurchargeID = holidaySurchargeID;
     }
 
+    public String getHolidayName() {
+        return holidayName;
+    }
+
+    public void setHolidayName(String holidayName) {
+        this.holidayName = holidayName;
+    }
+
+    public Date getHolidayDate() {
+        return holidayDate;
+    }
+
+    public void setHolidayDate(Date holidayDate) {
+        this.holidayDate = holidayDate;
+    }
+
+    public double getHolidaySurchargePrice() {
+        return holidaySurchargePrice;
+    }
+
+    public void setHolidaySurchargePrice(double holidaySurchargePrice) {
+        this.holidaySurchargePrice = holidaySurchargePrice;
+    }
+
+    
+    
     @Override
     public int hashCode() {
         int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
+        hash += (holidaySurchargeID != null ? holidaySurchargeID.hashCode() : 0);
         return hash;
     }
 
@@ -45,7 +76,7 @@ public class HolidaySurcharge implements Serializable {
             return false;
         }
         HolidaySurcharge other = (HolidaySurcharge) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+        if ((this.holidaySurchargeID == null && other.holidaySurchargeID != null) || (this.holidaySurchargeID != null && !this.holidaySurchargeID.equals(other.holidaySurchargeID))) {
             return false;
         }
         return true;
@@ -53,7 +84,7 @@ public class HolidaySurcharge implements Serializable {
 
     @Override
     public String toString() {
-        return "entity.HolidaySurcharge[ id=" + id + " ]";
+        return "entity.HolidaySurcharge[ holidaySurchargeID=" + holidaySurchargeID + " ]";
     }
     
 }
