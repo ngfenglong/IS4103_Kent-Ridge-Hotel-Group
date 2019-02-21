dfefb22547915f3eb9de5bc94a4a29fa2389db27
Feng Long <fenglong1502@Hotmail.com>
2019-02-21 13:20:48 +0800

    Compilation

---
+++ KRHG/KRHG-war/src/java/managedBean/HotelManagedBean.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managedBean;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+
+/**
+ *
+ * @author fengl
+ */
+@ManagedBean
+@ViewScoped
+public class HotelManagedBean implements Serializable{
+
+    /**
+     * Creates a new instance of NewJSFManagedBean
+     */
+    
+    
+    
+    public HotelManagedBean() {
+    }
+    
+    
+    
+}
