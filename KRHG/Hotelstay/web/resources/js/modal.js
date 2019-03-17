863d8508c9b1a9abbdccb9136549a194ceedfd7e
Clarisselsm <limclarisse96@gmail.com>
2019-03-18 02:26:05 +0800

    fixed min-max timepicker and datepicker issue.

--- KRHG/Hotelstay/web/resources/js/modal.js
+++ KRHG/Hotelstay/web/resources/js/modal.js
@@ -2,9 +2,9 @@ jQuery(document).ready(function($){
 	//open popup
 	$('.cd-popup-trigger').on('click', function(event){
 		event.preventDefault();
-		$('.cd-popup').addClass('is-visible');
+		$('#modal').addClass('is-visible');
 	});
-	
+  
 	//close popup
 	$('.cd-popup').on('click', function(event){
 		if( $(event.target).is('.cd-popup-close') || $(event.target).is('.cd-popup') ) {
