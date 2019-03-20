95e6427cbd75fa01f18e0bf60c9f1598d472fef8
Derek <dk3498@gmail.com>
2019-03-21 02:37:57 +0800

    Add a new RESTFUL api link for searching for House Keeping Order based on level

--- KRHG/KRHG-war/src/java/webservices/restful/HousekeepingordersResource.java
+++ KRHG/KRHG-war/src/java/webservices/restful/HousekeepingordersResource.java
@@ -23,6 +23,8 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import sessionBeans.HouseKeepingOrderSessionLocal;
@@ -35,19 +37,16 @@ import sessionBeans.HouseKeepingOrderSessionLocal;
 @Path("housekeepingorders")
 public class HousekeepingordersResource {
 
-
     HouseKeepingOrderSessionLocal houseKeepingOrderSession = lookupHouseKeepingOrderSessionLocal();
 //    @EJB
 //    private HouseKeepingOrderSessionLocal houseKeepingOrderSession;
-    
-    
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<HouseKeepingOrder> getAllHouseKeepingOrders() {
-        
-        System.out.println("Running 1112");
-        
+
+        System.out.println("Running 55");
+
         try {
             List<HouseKeepingOrder> getList = houseKeepingOrderSession.getAllHouseKeepingOrder();
             List<HouseKeepingOrder> returnList = new ArrayList<>();
@@ -86,6 +85,37 @@ public class HousekeepingordersResource {
             throw new RuntimeException(ne);
         }
 
+    }
+
+    @GET
+    @Path("/query")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response searchHouseKeepingOrder(@QueryParam("level") String level) throws NoResultException {
+        System.out.println("444Searching based on level: " + level);
+        if (level != null) {
+            List<HouseKeepingOrder> getList = houseKeepingOrderSession.getHouseKeepingOrderByLevel(Integer.parseInt(level));
+//            List<HouseKeepingOrder> returnList = new ArrayList<>();
+            List<HouseKeepingOrder> returnList = new ArrayList<>();
+            for (HouseKeepingOrder h : getList) {
+                HouseKeepingOrder tempHouseKeepingOrder = new HouseKeepingOrder();
+                tempHouseKeepingOrder.setHouseKeepingOrderID(h.getHouseKeepingOrderID());
+                tempHouseKeepingOrder.setRoom(h.getRoom());
+                Room tempRoom = new Room();
+                tempRoom.setRoomID(h.getRoom().getRoomID());
+                tempRoom.setRoomNumber(h.getRoom().getRoomNumber());
+                tempRoom.setStatus(h.getRoom().getStatus());
+                tempHouseKeepingOrder.setRoom(tempRoom);
+                tempHouseKeepingOrder.setStatus(h.getStatus());
+                tempHouseKeepingOrder.setSpecialRequest(h.getSpecialRequest());
+                tempHouseKeepingOrder.setLevel(h.getLevel());
+                returnList.add(tempHouseKeepingOrder);
+            }
+            GenericEntity<List<HouseKeepingOrder>> entity = new GenericEntity<List<HouseKeepingOrder>>(returnList) {};
+            return Response.status(200).entity(entity).build();
+//            return returnList;
+        } else {
+            return null;
+        }
     }
 
 }
