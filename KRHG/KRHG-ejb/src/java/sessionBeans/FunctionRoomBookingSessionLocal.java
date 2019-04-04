417f23412b6a81bb7fb4793aebf973e584560b6f
anselmdm <deadapple@live.com>
2019-04-04 19:42:23 +0800

    FunctionRoomBooking Sessionbeans

---
+++ KRHG/KRHG-ejb/src/java/sessionBeans/FunctionRoomBookingSessionLocal.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sessionBeans;
+
+import entity.FoodOrder;
+import error.NoResultException;
+import java.util.List;
+import javax.ejb.Local;
+
+@Local
+public interface FunctionRoomBookingSessionLocal {
+        
+    public List<FoodOrder> getAllFoodOrder();
+    public FoodOrder getAllFoodOrderByID(Long foodOrderID) throws NoResultException;
+    public void updateFoodOrder (FoodOrder foodOrder) throws NoResultException;
+    public void deleteFoodOrder (Long foodOrderID) throws NoResultException;
+    public void createFoodOrder (FoodOrder foodOrder);
+}
